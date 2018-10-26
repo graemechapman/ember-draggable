f3e42a8e18289e2644a9890df95fb0ebb2cdae46
Graeme Chapman <graeme.chapman@infinitycloud.com>
2018-10-26 23:03:40 +0100

    Add missing docs and default params

--- addon/components/draggable-list.js
+++ addon/components/draggable-list.js
@@ -6,7 +6,7 @@ import EmberObject from '@ember/object';
 import { htmlSafe } from '@ember/template';
 import { computed, setProperties } from '@ember/object';
 
-import { isPresent, typeOf } from '@ember/utils';
+import { isPresent, typeOf, tryInvoke } from '@ember/utils';
 import { assign } from '@ember/polyfills';
 
 export const EVENT_ITEM_DROPPED = Symbol('item dropped');
@@ -16,15 +16,52 @@ export const EVENT_ITEM_GRAB = Symbol('item grabbed');
 export default Component.extend({
   layout,
 
+  /**
+   * Items to render in the list
+   * @type {array|object|null}
+   */
   items: null,
+
+  /**
+   * Events property used to communicate with child components
+   * @type {object}
+   */
   events: null,
+
+  /**
+   * Y coordinates of the mouse cursor
+   * @type {number|null}
+   */
   mouseY: null,
+
+  /**
+   * Index of the currently active item
+   * @type {number|null}
+   */
   activeIndex: null,
-  dropTarget: null,
-  mouseUpListener: null,
-  groupTarget: null,
+
+  /**
+   * Active group (if in a grouped list)
+   * @type {string|null}
+   */
   activeGroup: null,
 
+  /**
+   * Index of the current drop target
+   * @type {number|null}
+   */
+  dropTarget: null,
+
+  /**
+   * The current drop target group
+   * @type {string|null}
+   */
+  groupTarget: null,
+
+  /**
+   * Sets up event listeners
+   * @override
+   */
   init() {
     this._super(...arguments);
 
@@ -33,58 +70,110 @@ export default Component.extend({
     this.events.on(EVENT_ITEM_DROPPED, () => {
       if (isPresent(this.dropTarget) && isPresent(this.activeIndex)) {
         if (this.isGroupedList && isPresent(this.groupTarget) && isPresent(this.activeGroup)) {
-          return this.onChange(this.moveObjectElement(this.items, this.activeIndex, this.activeGroup, this.dropTarget, this.groupTarget));
+          return tryInvoke(this, 'onChange', [this.moveObjectElement(this.items, this.activeIndex, this.activeGroup, this.dropTarget, this.groupTarget)]);
         }
 
-        this.onChange(this.moveArrayElement(this.items, this.activeIndex, this.dropTarget));
+        tryInvoke(this, 'onChange', [this.moveArrayElement(this.items, this.activeIndex, this.dropTarget)]);
       }
 
       this.set('dropTarget', null);
     });
 
-    this.events.on(EVENT_ITEM_HOVER, (itemId, group = null) => {
-      this.set('dropTarget', itemId);
-      this.set('groupTarget', group);
+    this.events.on(EVENT_ITEM_HOVER, (dropTarget, groupTarget = null) => {
+      this.setProperties({
+        dropTarget,
+        groupTarget
+      });
     });
 
-    this.events.on(EVENT_ITEM_GRAB, (itemId, element, group = null) => {
-      this.set('activeIndex', itemId);
-      this.set('activeGroup', group);
-      this.set('elementHeight', element.offsetHeight);
+    this.events.on(EVENT_ITEM_GRAB, (activeIndex, element, activeGroup = null) => {
+      this.setProperties({
+        activeIndex,
+        activeGroup,
+        elementHeight: element.offsetHeight
+      });
 
       // add event listener on the whole document, this stops the component bugging out if mouse is moved outside the element
-      this.set('mouseUpListener', document.addEventListener('mouseup', () => {
+      document.addEventListener('mouseup', this.mouseUpListener);
+      document.addEventListener('mouseleave', this.mouseUpListener);
+    });
+
+    // create event listener to bind to mouse events on the document
+    this.set('mouseUpListener', {
+      handleEvent: () => {
         this.events.trigger(EVENT_ITEM_DROPPED);
 
-        this.set('activeIndex', null);
-        this.set('activeGroup', null);
-        this.set('groupTarget', null);
-        this.set('dropTarget', null);
+        this.setProperties({
+          activeIndex: null,
+          activeGroup: null,
+          groupTarget: null,
+          dropTarget: null
+        });
 
         document.removeEventListener('mouseup', this.mouseUpListener);
-      }));
+        document.removeEventListener('mouseleave', this.mouseUpListener);
+      }
     });
   },
 
+  /**
+   * Removes event listeners from document if this element is removed
+   */
+  willDestroy() {
+    this._super(...arguments);
+
+    document.removeEventListener('mouseup', this.mouseUpListener);
+    document.removeEventListener('mouseleave', this.mouseUpListener);
+  },
+
+  /**
+   * Event listener for document mouse up events
+   * @type {object}
+   */
+  mouseUpListener: null,
+
+  /**
+   * Whether the list provided is grouped
+   */
   isGroupedList: computed('items', {
     get() {
       return typeOf(this.items) === 'object';
     }
   }),
 
+  /**
+   * Returns a new array with a single element moved within that array
+   * @param  {array}  array
+   * @param  {number} from  index to move from
+   * @param  {to}     to    index to move to
+   */
   moveArrayElement(array, from, to) {
     array = [ ...array ];
 
-    const modifier = (to > from ? 0 : 1);
+    // adjust the index, to account for the difference between drop targets - these are indexed from -1
+    // as we need to allow items to be placed before the item at index 0
+    to = (to < from ? to + 1 : to);
 
-    array.insertAt(to + modifier, array.splice(from, 1)[0]);
+    if (from > array.length || to > array.length) {
+      return array;
+    }
 
-    return array;
+    return array.insertAt(to, array.splice(from, 1)[0]);
   },
 
-  moveObjectElement(originalObject, from, fromGroup, to, toGroup) {
-    const object = assign({}, originalObject);
+  /**
+   * Returns a new object with the item from the provided group & key moved to the target group & key
+   * @param  {object} object
+   * @param  {number} from      index to move from
+   * @param  {string} fromGroup group name to move from
+   * @param  {number} to        index to move to
+   * @param  {string} toGroup   group name to move to
+   * @return {object}
+   */
+  moveObjectElement(object, from, fromGroup, to, toGroup) {
+    object = assign({}, object);
 
+    // if it's moving within the same group, just move within that array
     if (fromGroup === toGroup) {
       object[fromGroup] = this.moveArrayElement(object[fromGroup], from, to);
 
@@ -101,10 +190,24 @@ export default Component.extend({
     return object;
   },
 
+  /**
+   * Handles mouse events to get the pointer location
+   * @param  {MouseEvent} event
+   */
   mouseMove(event) {
     const rect = this.element.getBoundingClientRect();
 
-    this.set('mouseY', event.clientY - rect.top);
+    let top = event.clientY - rect.top;
+
+    if (top < 0) {
+      top = 0;
+    }
+
+    if (top > rect.height) {
+      top = rect.height;
+    }
+
+    this.set('mouseY', top);
   },
 
   style: computed('mouseY', {
--- addon/components/draggable-list/item.js
+++ addon/components/draggable-list/item.js
@@ -7,18 +7,83 @@ import { EVENT_ITEM_GRAB, EVENT_ITEM_HOVER } from '../draggable-list';
 export default Component.extend({
   layout,
 
+  /**
+   * Item index
+   * @type {number|null}
+   */
   index: null,
-  activeIndex: false,
+
+  /**
+   * Index of the currently active item
+   * @type {number|null}
+   */
+  activeIndex: null,
+
+  /**
+   * Item's group (if in a grouped list)
+   * @type {string|null}
+   */
   group: null,
+
+  /**
+   * Active group (if in a grouped list)
+   * @type {string|null}
+   */
   activeGroup: null,
+
+  /**
+   * Inactive items can't be dragged
+   * @type {Boolean}
+   */
   inactive: false,
 
+  /**
+   * Index of the current drop target
+   * @type {number|null}
+   */
+  dropTarget: null,
+
+  /**
+   * The current drop target group
+   * @type {string|null}
+   */
+  groupTarget: null,
+
+  /**
+   * Events property used to communicate with parent component
+   * @type {object}
+   */
+  events: null,
+
+  /**
+   * Whether this element is currently being dragged
+   * @type {boolean}
+   */
   isDragging: computed('index', 'activeIndex', 'group', 'activeGroup', {
     get() {
       return ((this.activeIndex === this.index) && (this.activeGroup === this.group));
     }
   }),
 
+  /**
+   * Whether this item is visible.
+   * Set to false if this item is being dragged, and the current drop target is not this item's current location.
+   * @type {boolean}
+   */
+  isVisible: computed('isDragging', 'dropTarget', {
+    get() {
+      if (this.isDragging) {
+        return (this.dropTarget === null || this.dropTarget === this.index && this.groupTarget === this.group);
+      }
+
+      return true;
+    }
+  }),
+
+  /**
+   * Starts dragging an element on mouse down.
+   * @param  {MouseEvent} event
+   */
   mouseDown(event) {
     if (this.inactive) {
       return;
@@ -29,6 +94,10 @@ export default Component.extend({
     this.events.trigger(EVENT_ITEM_GRAB, this.index, this.element, this.group);
   },
 
+  /**
+   * Handles mouse events to detect the active drop target.
+   * @param  {MouseEvent} event
+   */
   mouseMove(event) {
     if (this.activeIndex === null) {
       return;
